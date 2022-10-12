7069d9f55f9fc3df7e50ab60def0fb94830fa723
Garima Chhikara <garima@Garimas-MacBook-Pro.local>
2022-10-12 13:19:20 +0530

    Trees

--- Trees/BinaryTree.cpp
+++ Trees/BinaryTree.cpp
@@ -27,17 +27,37 @@ class BinaryTree
     BinaryTree()
     {
         
-        root = new Node(10) ;
-        root->left = new Node(20) ;
-        root->right = new Node(30) ;
-        root->left->left = new Node(40) ;
-        root->left->right = new Node(50) ;
-        root->right->right = new Node(60) ;
+        root = new Node(-10) ;
+        root->left = new Node(-20) ;
+        root->right = new Node(-30) ;
+        root->left->left = new Node(-40) ;
+        root->left->right = new Node(-50) ;
+        root->right->right = new Node(-60) ;
     }
 
     void display()
     {
         display(root) ;
+    }
+
+    int size()
+    {
+        return size(root) ;        
+    }
+
+    int ht()
+    {
+        return ht(root) ;
+    }
+
+    int maximum()
+    {
+        return maximum(root) ;
+    }
+
+    bool find(int item)
+    {
+        return find(root, item) ;
     }
 
     private :
@@ -72,11 +92,75 @@ class BinaryTree
 
     }
 
+    int size(Node *node)
+    {
+        if(node == NULL)
+            return 0 ;
+
+        // left
+        int ls = size(node->left) ;
+
+        // right
+        int rs = size(node->right) ;
+
+        // work
+        return ls + rs + 1 ;
+    }
+
+    int ht(Node *node)
+    {
+        if(node == NULL)
+            return -1 ;
+
+        int lh = ht(node->left) ;
+
+        int rh = ht(node->right) ;
+
+        return max(lh, rh) + 1 ;
+    }
+
+    int maximum(Node *node)
+    {
+        if(node == NULL)
+            return INT_MIN ;
+
+        int lm = maximum(node->left) ;
+        int rm = maximum(node->right) ;
+
+        return max(node->data , max(lm,rm)) ;
+    }
+
+    bool find(Node *node, int item)
+    {
+        if(node == NULL)
+            return false ;
+
+        if(node->data == item)
+            return true ;
+
+        bool lf = find(node->left, item) ;
+
+        if(lf)
+            return true ;
+            
+        bool rf = find(node->right, item) ;
+
+        return lf || rf ;
+
+    }
+
+
 } ;
+
 
 int main()
 {
     BinaryTree bt ;
     bt.display() ;
+    cout << bt.size() << endl;
+    cout << bt.ht() << endl ;
+    cout << bt.maximum()  << endl ;
+    cout << bt.find(-50) << endl ;
+
     return 0 ;
 }
